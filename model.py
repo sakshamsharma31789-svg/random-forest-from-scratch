7a38d3fc4eeaa95aa3cb599567473b6d40459180
sakshamsharma31789-svg <sakshamsharma31789@gmail.com>
2026-07-11 19:20:31 +0530

    Step 3: split_score

--- model.py
+++ model.py
@@ -28,8 +28,14 @@ def split_dataset(features, labels, feature_index, threshold):
     return features[mask],labels[mask],features[~mask],labels[~mask]
     pass
 
-# Step 3 - split_score (not yet solved)
-# TODO: implement
+# Step 3 - split_score
+def split_score(parent_labels, left_labels, right_labels):
+    # TODO: return a score where higher means the children are purer than the parent.
+    n = len(parent_labels)
+    w_l = len(left_labels) / n
+    w_r = len(right_labels) / n 
+    return impurity(parent_labels) - ((w_l * impurity(left_labels)) + (w_r*impurity(right_labels)))
+    pass
 
 # Step 4 - best_split (not yet solved)
 # TODO: implement
