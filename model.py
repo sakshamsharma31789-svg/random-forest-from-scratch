406cd846bae5025f26b263dddabca4e1524b42de
sakshamsharma31789-svg <sakshamsharma31789@gmail.com>
2026-07-27 01:22:09 +0530

    Step 11: feature_subset

--- model.py
+++ model.py
@@ -169,8 +169,14 @@ def bootstrap_sample(features, labels, rng):
 
     pass
 
-# Step 11 - feature_subset (not yet solved)
-# TODO: implement
+# Step 11 - feature_subset
+import numpy as np
+
+def feature_subset(num_features, num_to_pick, rng):
+    # TODO: return num_to_pick distinct random feature indices from range(num_features) using rng.
+    return rng.choice(num_features,size=num_to_pick,replace=False)
+
+    pass
 
 # Step 12 - train_forest (not yet solved)
 # TODO: implement
